52dea9658a1b70395bddc79a15788e22c0832e1d
sheruaravindreddy <sheru.reddy@iitgn.ac.in>
2017-02-26 15:41:25 +0530

    Delete importdate.py

--- importdate.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-i = 1181
-while i<=1200:
-    prev_date = (datetime.now() - timedelta(i)).strftime('%Y-%m-%d')
-    print (i)
-    print (prev_date)
-    i+=1

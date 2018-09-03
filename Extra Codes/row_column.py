d65074e6a94aead9adfa569be32b889ef7f1ddac
sheruaravindreddy <sheru.reddy@iitgn.ac.in>
2018-09-03 11:26:15 +0530

    Delete row_column.py

--- Extra Codes/row_column.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('output.csv', 'w', newline='') as csvfile:
-    output = ['USA','IND']
-    d = csv.writer(csvfile, delimiter=',', quotechar='|', 
-    quoting=csv.QUOTE_MINIMAL)  
-    d.writerow(output)
-    d.writerow(output)
